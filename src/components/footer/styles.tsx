382247c58246436e27d1dcfcfc2189dbb6da1f1c
Sarah Schneider <mureb_rosa@hotmail.com>
2023-01-27 16:53:33 -0300

    responsive: footer

--- src/components/footer/styles.tsx
+++ src/components/footer/styles.tsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width: fit-content;
+    width: 100%;
+    max-width: 80%;
     height: fit-content;
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 0.8rem;
     margin: 6rem auto 1.2rem;
+    text-align: center;
 `
 
 export const Name = styled.p`
