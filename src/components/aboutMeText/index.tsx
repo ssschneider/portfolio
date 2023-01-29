ac687b218d299730f24a4496dfd52d0029376c03
Sarah Schneider <mureb_rosa@hotmail.com>
2023-01-29 13:26:16 -0300

    add about me text

--- src/components/aboutMeText/index.tsx
+++ src/components/aboutMeText/index.tsx
@@ -5,13 +5,17 @@ const AboutMeText = () => {
         <Wrapper>
             <SectionTitle>Meu nome é Sarah,</SectionTitle>
 
-            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus temporibus accusamus molestiae maxime? Natus nobis modi saepe. Eaque, earum debitis ea sit iure delectus voluptatem a explicabo quidem unde facilis?</Text>
+            <Text>
+                e eu sou uma pessoa simplesmente apaixonada por aprender coisas novas. Desde criança eu sempre fui curiosa, sem medo de fazer perguntas para tentar entender as coisas e isso não mudou, mas foi potencializado conforme eu fui ficando mais velha e pude começar a explorar o mundo autodidata.
+            </Text>
 
-            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus temporibus accusamus molestiae maxime? Natus nobis modi saepe. Eaque, earum debitis ea sit iure delectus voluptatem a explicabo quidem unde facilis?</Text>
+            <Text>
+                Minha trajetória até aqui reforça o quanto eu não tenho medo de tentar e errar, além de me conhecer cada vez mais. Tenho fortes crenças de que nunca sabemos tudo o que tem para saber de algum tópico e sempre há espaço para crescimento, mesmo que não tenhamos essa percepção. Por isso, acredito que feedback e uma comunicação efetiva são tão importantes para o crescimento pessoal e profissional.
+            </Text>
 
-            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus temporibus accusamus molestiae maxime? Natus nobis modi saepe. Eaque, earum debitis ea sit iure delectus voluptatem a explicabo quidem unde facilis?</Text>
-
-            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus temporibus accusamus molestiae maxime? Natus nobis modi saepe. Eaque, earum debitis ea sit iure delectus voluptatem a explicabo quidem unde facilis?</Text>
+            <Text>
+                Conheci o mundo da programação em julho de 2022 e, desde então, venho me dedicando integralmente para me tornar uma Desenvolvedora Front-End de excelência, me aprofundando cada vez mais no código. Para me auxiliar nesse percurso, além de materiais em português, utilizo da minha fluência em inglês e espanhol (além dos estudos em alemão) para me aprimorar cada vez mais.
+                </Text>
         </Wrapper>
     )
 }
